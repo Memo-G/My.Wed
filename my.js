9e81344a483b81f57fe24ab7adeaeafca5e5f7d7
Memo-G <43025878+Memo-G@users.noreply.github.com>
2018-09-10 11:49:48 +0900

    Update my.js

--- my.js
+++ my.js
@@ -3,7 +3,7 @@
     var header = document.getElementById('header');
     var deg = 0;
     function rotateHeader() {
-        deg = deg + 0.00001;
+        deg = deg + 0.0000000000000000000000000000000000000000000000001;
         header.style.transform = 'rotateX(' + deg +  'deg)';
     }
     setInterval(rotateHeader, 1);
